fe5b5aea1d8a200c6c95b9d5077985cac696d510
Rodrigo <rss.999999999@gmail.com>
2023-10-07 11:24:04 -0300

    Initial commit

---
+++ src/rowSumOddNumbers.java
@@ -0,0 +1,14 @@
+class RowSumOddNumbers {
+    public static int rowSumOddNumbers(int n) {
+
+        for  (int i=1; i<n;i++){
+            int x = (n*(n-1))+1;
+            x = x + 2;
+
+
+        }
+
+        return x;
+
+    }
+}
