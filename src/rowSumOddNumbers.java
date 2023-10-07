12954c74d92a4ac777af2c3d17284725d17ef87e
Rodrigo <rss.999999999@gmail.com>
2023-10-07 11:25:15 -0300

    Add description

--- src/rowSumOddNumbers.java
+++ src/rowSumOddNumbers.java
@@ -1,14 +1,30 @@
+/*
+Given the triangle of consecutive odd numbers:
+
+        1
+        3     5
+        7     9    11
+        13    15    17    19
+        21    23    25    27    29
+        ...
+        Calculate the sum of the numbers in the nth row of this triangle (starting at index 1) e.g.: (Input --> Output)
+
+        1 -->  1
+        2 --> 3 + 5 = 8
+*/
+
+
 class RowSumOddNumbers {
     public static int rowSumOddNumbers(int n) {
+        int x = (n * (n - 1)) + 1;  //here we are calculating the first number of the given row
+        int sum = 0;
 
-        for  (int i=1; i<n;i++){
-            int x = (n*(n-1))+1;
-            x = x + 2;
-
-
+        //here we are adding all the numbers of the given row
+        for (int i = 0; i < n; i++) {
+            sum += x;
+            x += 2;
         }
 
-        return x;
-
+        return sum;
     }
 }
--- tests/RowSumOddNumbersTest.java
+++ tests/RowSumOddNumbersTest.java
@@ -1,4 +1,16 @@
 import static org.junit.Assert.*;
+import org.junit.Test;
+import java.util.Random;
+
+
 public class RowSumOddNumbersTest {
-  
+
+    @Test
+    public void test1() {
+        Random rnd = new Random();
+        for(int i=0;i<50;i++){
+            int n = rnd.nextInt(500)+1;
+            assertEquals((int) Math.pow(n,3), RowSumOddNumbers.rowSumOddNumbers(n));
+        }
+    }
 }
